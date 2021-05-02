cfbc579655d894b0c0395038a6f9312470cf95f0
tsuchinaga <woocun@gmail.com>
2021-05-03 04:40:58 +0900

    initial commit

---
+++ PauseAtNewDemands/PauseAtNewDemands.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using VoxelTycoon;
+using VoxelTycoon.Cities;
+using VoxelTycoon.Modding;
+
+namespace PauseAtNewDemands
+{
+    public class PauseAtNewDemands : Mod
+    {
+        private readonly HashSet<CityDemand> _checkedCityDemands = new HashSet<CityDemand>();
+
+        protected override void OnGameStarting()
+        {
+            var demands = Demands();
+            foreach (var demand in demands)
+            {
+                _checkedCityDemands.Add(demand);
+            }
+        }
+
+        protected override void OnUpdate()
+        {
+            var demands = Demands();
+            foreach (var demand in demands)
+            {
+                if (_checkedCityDemands.Add(demand))
+                {
+                    TimeManager.Current.TimeScale = 0.0f;
+                }
+            }
+        }
+
+        private static IEnumerable<CityDemand> Demands()
+        {
+            var list = new List<CityDemand>();
+            
+            var cities = CityManager.Current.Cities;
+            for (var i = 0; i < cities.Count; i++)
+            {
+                var city = cities[i];
+                if (city.Region.State != RegionState.Unlocked) continue;
+
+                for (var j = 0; j < city.Demands.Count; j++)
+                {
+                    list.Add(city.Demands[j]);
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# VoxelTycoon Mod "Pause at New Demands"
+
+VoxelTycoonのMODで、新しい需要が発生した際にPauseをかけるだけのMOD
+
+
+## MODをダウンロード
+
+[こちらからダウンロードしてください](https://gitlab.com/tsuchinaga/voxel-tycoon-mod-pause-at-new-demands/-/jobs/artifacts/master/download?job=build)
